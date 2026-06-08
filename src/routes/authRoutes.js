586a2fc81da68b43ce6feab3ffe22817250e91ee
storygo679149-afk <storygo679149@gmail.com>
2026-06-08 16:40:36 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,19 @@
-// src/routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { register, verifyOTP, resendOTP, login, logout } = require('../controllers/authController');
+const {
+  register,
+  verifyOTP,
+  resendOTP,
+  login,
+  verifyLoginOTP,
+  logout,
+} = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/verify-otp', verifyOTP);
 router.post('/resend-otp', resendOTP);
 router.post('/login', login);
+router.post('/verify-login-otp', verifyLoginOTP);
 router.post('/logout', logout);
 
 module.exports = router;
