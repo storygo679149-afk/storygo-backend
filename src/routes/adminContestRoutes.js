6cc9c490f6b8a4f8d87250e94b3a3af7efa5bd7f
storygo679149-afk <storygo679149@gmail.com>
2026-06-11 13:00:22 +0530

    Update adminContestRoutes.js

--- src/routes/adminContestRoutes.js
+++ src/routes/adminContestRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const { authenticate, authorizeAdmin } = require('../middleware/authenticate');
 const adminContest = require('../controllers/adminContestController');
 
+// All admin contest routes require authentication and admin role
 router.use(authenticate);
 router.use(authorizeAdmin);
 
+// Contest CRUD
 router.post('/contests', adminContest.createContest);
 router.get('/contests', adminContest.getContests);
 router.get('/contests/:id', adminContest.getContest);
 router.put('/contests/:id', adminContest.updateContest);
 router.delete('/contests/:id', adminContest.deleteContest);
+
+// Submissions & ratings
 router.get('/contests/:id/submissions', adminContest.getSubmissionsForContest);
 router.post('/contests/:contestId/submissions/:submissionId/rate', adminContest.rateSubmission);
 router.post('/contests/:id/determine-winner', adminContest.determineWinner);
